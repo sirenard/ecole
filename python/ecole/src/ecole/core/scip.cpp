3b94ddc56d15d00f9470abfb1263337c62904fae
Simon Renard <renardsimon@skynet.be>
2025-03-27 12:59:49 +0100

    Remove direct scip pointer access

--- python/ecole/src/ecole/core/scip.cpp
+++ python/ecole/src/ecole/core/scip.cpp
@@ -161,12 +161,6 @@ void bind_submodule(py::module_ m) {
 			// is alive, as PyScipOpt is a view on the ecole Model.
 			py::keep_alive<0, 1>())
 
-		.def(
-			"get_scip_ptr",
-			[](scip::Model& model) {
-				return reinterpret_cast<long>(model.get_scip_ptr());
-			})
-
 		.def("set_messagehdlr_quiet", &Model::set_messagehdlr_quiet, py::arg("quiet"))
 
 		.def_property("name", &Model::name, &Model::set_name)
